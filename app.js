bf3641fd5232f1eb28247238988b70005fc48a1e
ms109420 <ms109420@naver.com>
2022-02-13 22:24:15 +0900

    Event 두번째

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const title = document.querySelector("div.hello:first-child h1");
 
+console.dir(title);
+
+
+
 function handleTitleClick(){
     title.style.color = " blue"
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
 
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
