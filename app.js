90bc407cf1e2e170078bd65c9be1b4a0a27dc4a4
ms109420 <ms109420@naver.com>
2022-02-13 16:53:15 +0900

    click event

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-const title = document.querySelectorAll(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
+function handleTitleClick(){
+    title.style.color = " blue"
+}
 
-/*
-- querySelector : element를 CSS selector방식 검색(ex. h1:first-child)***가장 많이 쓰임
-- getElementById: 하나의 값 반환
-- getElementsByClassName() : 많은 element를 가져올때 씀(배열)
-- getElementsByTagName() : name을 할당 가능(배열)
+title.addEventListener("click", handleTitleClick);
 
-const title = document.querySelectorAll("#hello");
-const title = document.getElementById("hello");
-둘의 코드는 같다 querySelectorAll는 CSS selector 자체를 전달
-#이 붙는 이유는 id를 가져온다고 명시해줘야 하기 떄문이다
-
-*/
