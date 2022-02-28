d763e4eb8343d326dc405cc887ef95384e0eff93
ms109420 <ms109420@naver.com>
2022-02-28 17:59:33 +0900

    preventDefault 함수

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+const loginForm   = document.querySelector("#login-form");
 const loginIput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const username = loginIput.value;
-    console.log(username);
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(tomato) {
+    tomato.preventDefault(); //브라우저가 새로고침 되는것을 막아줌
+    console.log(tomato);
+}  
+
+//tomato(event)는 submit의 정보 및 기능을 담는 object이다
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
+
 }
 
-loginButton.addEventListener("click", onLoginBtnClick );
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
