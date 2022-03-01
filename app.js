81c0455c16fae5816fd779d16c6f6c30483a1d46
ms109420 <ms109420@naver.com>
2022-03-02 01:34:04 +0900

    username 저장

--- app.js
+++ app.js
@@ -2,16 +2,30 @@ const loginForm   = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); //브라우저가 새로고침 되는것을 막아줌
     loginForm.classList.add("hidden"); //입력하면 입력창을 없애줌
-    const username = loginInput.value; //입력한 유저 이름을 변수에 저장
-    greeting.innerText = `Hello  ${username}`; 
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);//로컬에 저장
+    paintGreetings(username);
 }  
 
-loginForm.addEventListener("submit", onLoginSubmit);
 
+function paintGreetings(username){
+    greeting.innerText = `Hello  ${username}`; 
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){ //입력값이 없으면 form을 보이게 함
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit); //입력값 기다림
+
+} else {
+    paintGreetings(saedUsername);
+    
+}
